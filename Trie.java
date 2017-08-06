771ccbb5dedad70257801b6ed17e82917e866b4d
Samuel Wasserman <sambwasserman@gmail.com>
2017-08-06 10:42:37 -0400

    ommision

--- Trie.java
+++ Trie.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
  * This class implements a compressed trie. Each node of the tree is a CompressedTrieNode, with fields for
  * indexes, first child and sibling.
  * 
- * @author Sesh Venugopal
  *
  */
 public class Trie {
